7b0282829e07613c07ae21b1ebc338a1effd845c
chetha123 <54464707+chetha123@users.noreply.github.com>
2023-03-21 19:43:50 +0530

    Update clock_freq_generator.sv

--- clock_freq_generator.sv
+++ clock_freq_generator.sv
@@ -1,13 +1,34 @@
+// Code your testbench here
+// or browse Examples
 module clokck_freq_gen();
-  bit clock;
-  parameter DUTY_CYCLE=50;
+  reg clock;
+  parameter DUTY_CYCLE=70;
   parameter TIME_PERIOD=10;
   parameter ACTIVE_TIME_WIDTH=(DUTY_CYCLE*TIME_PERIOD)/100;
   parameter NON_ACTIVE_TIME_WIDTH=TIME_PERIOD-ACTIVE_TIME_WIDTH;
   
-  always clock=1'b1;
-  #ACTIVE_TIME_WIDTH ;
-  always clock=1'b0;
-  #NON_ACTIVE_TIME_WIDTH;
+  initial 
+    begin 
+	    
+        #1000; 
+		
+        $finish; 
+    end 
+  
+  always begin
+  
+  clock=1'b0;
+  #(NON_ACTIVE_TIME_WIDTH);
+    
+  clock=1'b1;
+  # (ACTIVE_TIME_WIDTH);
+  
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+  
   
 endmodule
