ba029a2e670c11dae9636bd157b6a2dd143b8b10
chetha123 <54464707+chetha123@users.noreply.github.com>
2023-04-01 08:03:57 +0530

    Update power_of_two.sv

--- power_of_two.sv
+++ power_of_two.sv
@@ -1,17 +1,17 @@
 //genrate  array having random values which should be power of 2 and it should print randomnly not like 2,4,8,16...
 class power_of_two;
-  rand bit [15:0] bit_pattern[];
+  rand bit [15:0] pow[];
   
-  constraint size_arr_C { bit_pattern.size inside {[10:16]}; }  	     
+  constraint size_arr_C { pow.size inside {[10:16]}; }  	     
   constraint pattern_C { 
-    foreach(bit_pattern[i]) {      
-      bit_pattern[i]==2**i;     
+    foreach(pow[i]) {      
+      pow[i]==2**i;     
     }  }
       
    function print();
-    bit_pattern.shuffle();    
-    foreach(bit_pattern[i])
-      $write(" %0d", bit_pattern[i]); 
+    pow.shuffle();    
+    foreach(pow[i])
+      $write(" %0d", pow[i]); 
       $write("\n");  
     endfunction
 endclass 
