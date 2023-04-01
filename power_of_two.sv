70f599aed240650f4a153c9b31762d97e87c63c0
chetha123 <54464707+chetha123@users.noreply.github.com>
2023-04-01 08:01:35 +0530

    generate power_of_two
    generate  array having random values which should be power of 2 and it should print randomnly not like 2,4,8,16...

---
+++ power_of_two.sv
@@ -0,0 +1,24 @@
+//genrate  array having random values which should be power of 2 and it should print randomnly not like 2,4,8,16...
+class power_of_two;
+  rand bit [15:0] bit_pattern[];
+  
+  constraint size_arr_C { bit_pattern.size inside {[10:16]}; }  	     constraint pattern_C { 
+    foreach(bit_pattern[i]) {      
+      bit_pattern[i]==2**i;     
+    }  }
+      
+   function print();
+    bit_pattern.shuffle();    
+    foreach(bit_pattern[i])
+      $write(" %0d", bit_pattern[i]); 
+      $write("\n");  
+    endfunction
+endclass 
+    
+ module test;
+  power_of_two o = new();  
+   initial begin
+    o.randomize();    
+    o.print();
+  end
+endmodule
